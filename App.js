2d2078e84a4714e36752358d315d22460e05aeb2
IPG <ipg@IPGnoMacBook-puro.local>
2019-02-13 23:48:02 +0900

    feature: added unstated and created todo list

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import { Provider } from 'unstated'
 
 export default class App extends React.Component {
   state = {
@@ -19,10 +20,9 @@ export default class App extends React.Component {
       );
     } else {
       return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <Provider>
           <AppNavigator />
-        </View>
+        </Provider>
       );
     }
   }
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,22 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   StyleSheet,
   Text,
   Button,
-  View
-} from 'react-native';
+  View,
+  Dimensions,
+  TextInput,
+  AsyncStorage,
+  FlatList,
+} from 'react-native'
+import { Provider, Subscribe, Container } from 'unstated'
+import { PersistContainer } from 'unstated-persist'
 
-class StopWatch extends Component {
+
+class timerState extends PersistContainer {
+  persist = {
+    key: 'timerState',
+    version: 1,
+    storage: AsyncStorage,
+  }
+
+  state = {
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1, countHistory: [...this.state.countHistory, { lol: 77 }] });
+  }
+}
+
+class todoState extends PersistContainer {
+  persist = {
+    key: 'todoState',
+    version: 1,
+    storage: AsyncStorage,
+  }
+
+  state = {
+    todoList: [{title: 'todo1'}, {title: 'todo2'}],
+    doneList: [{title: 'done1'}, {title: 'done2'}],
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1, countHistory: [...this.state.countHistory, { lol: 77 }] });
+  }
+}
+
+const { height, width } = Dimensions.get('window')
+
+class HomeScreen extends Component {
   state = {
     timer: null,
     counter: 25 * 60,
     miliseconds: 0,
     startDisabled: true,
     stopDisabled: false,
+    isRest: false,
+    isRunning: false,
   }
 
   componentDidMount = () => {
-    this.start()
+    // this.start()
   }
 
   componentWillUnmount = () => {
@@ -27,18 +70,18 @@ class StopWatch extends Component {
     let timer = setInterval(() => {
       let num = (this.state.miliseconds + 1)
       let count = this.state.counter
-      
+
       if (this.state.miliseconds == 99 && this.state.counter !== 0) {
         count = (this.state.counter - 1)
         num = 0
       }
-      
+
       this.setState({
         counter: count,
         miliseconds: num
       })
     }, 0)
-    this.setState({ timer });
+    this.setState({ timer, isRunning: true });
   }
 
   onButtonStart = () => {
@@ -46,7 +89,9 @@ class StopWatch extends Component {
   }
 
   onButtonStop = () => {
+    console.warn(this.state.timer)
     clearInterval(this.state.timer);
+    this.setState({isRunning: false})
   }
 
   onButtonClear = () => {
@@ -58,20 +103,32 @@ class StopWatch extends Component {
   }
 
   render() {
-    const {counter} = this.state
+    const {timerState, todoState} = this.props
+    const { counter, isRest, isRunning } = this.state
     const minutes = Math.floor(counter / 60)
     const seconds = counter % 60
+
     return (
       <View style={styles.container}>
-        <View style={styles.counterWrapper}>
-        <Text style={styles.counterText}>{minutes}:{seconds}</Text>
+        <View style={styles.timerContainer, {backgroundColor: isRest ? 'green': 'red'}}>
+          <View style={styles.counterWrapper}>
+          <Text style={styles.counterText}>{minutes}:{seconds}</Text>
         </View>
         <View styles={styles.buttonWrapper}>
-          <Text onPress={() => this.start()}>開始</Text>
-          <Text onPress={() => this.onButtonStop()}>停止</Text>
-          <Text onPress={() => this.onButtonClear()}>リセット</Text>
+          {isRunning
+            ? <Text style={styles.stopButton} onPress={() => this.onButtonStop()}>停止</Text>
+            : <Text style={styles.startButton} onPress={() => this.start()}>開始</Text>}
+          <Text style={styles.resetButton} onPress={() => this.onButtonClear()}>リセット</Text>
         </View>
       </View>
+      <View style={styles.todoContainer}>
+        <TextInput />
+        <Text>TODO</Text>
+        {todoState.state.todoList.map(x => <Text>{x.title}</Text>)}
+        <Text>完了</Text>
+        {todoState.state.doneList.map(x => <Text>{x.title}</Text>)}
+      </View>
+      </View >
     )
   }
 }
@@ -80,14 +137,18 @@ class StopWatch extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
     flexDirection: 'column',
   },
+  timerContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: height / 4,
+    width: width,
+  },
   counterWrapper: {
-    flex: 2
   },
   counter: {
     fontSize: 60,
@@ -99,16 +160,30 @@ const styles = StyleSheet.create({
     fontSize: 50
   },
   buttonWrapper: {
-    flex: 1,
     flexDirection: 'row'
+  },
+  startButton: {
+    borderWidth: 1,
+  },
+  stopButton: {
+    borderWidth: 1,
+  },
+  resetButton: {
+    borderWidth: 1,
   },
   miniCounter: {
     fontSize: 20,
     position: 'relative',
     top: -32,
     right: -50
+  },
+  todoContainer: {
+    backgroundColor: 'blue'
   }
 });
 
-
-module.exports = StopWatch;
+export default App = () => (
+  <Subscribe to={[timerState, todoState]}>
+    {(timerState, todoState) => <HomeScreen timerState={timerState} todoState={todoState} />}
+  </Subscribe>
+)
